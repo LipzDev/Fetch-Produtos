9f3352c7799bc2a7425c3195e9b4e1d8dc62e3d1
LipzDev <filipe.gt1998@outlook.com>
2021-05-01 20:27:14 -0300

    Loading

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -4,27 +4,29 @@ import { useParams } from 'react-router';
 
 const Item = () => {
   const [item, setItem] = React.useState(null);
+  const [load, setLoad] = React.useState(false);
   const { id } = useParams();
 
   React.useEffect(() => {
     async function showItem(url) {
+      setLoad(true);
       const response = await fetch(url);
       const json = await response.json();
       setItem(json);
+      setLoad(false);
     }
     showItem(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
-
-  if (item === null) return null;
 
   return (
     <section className="item efeito">
       <div className="container">
         <div className="item__produto">
-          <img src={item.fotos[0].src} />
-          <h2>{item.nome}</h2>
-          <span>{item.preco}</span>
-          <p>{item.descricao}</p>
+          {load === true ? <h1>Carregando...</h1> : ''}
+          <img src={item && item.fotos[0].src} />
+          <h2>{item && item.nome}</h2>
+          <span>{item && item.preco}</span>
+          <p>{item && item.descricao}</p>
         </div>
       </div>
     </section>
